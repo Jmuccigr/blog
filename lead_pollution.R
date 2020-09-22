eea906fd83093e9c82d369d72a704106b54f77f3
John Muccigrosso <john@muccigrosso.org>
2020-09-22 09:44:04 +0200

    Tweak chart

--- lead_pollution.R
+++ lead_pollution.R
@@ -23,10 +23,10 @@ ymax <- floor(max(leaddata[3], na.rm=TRUE))
 startDate <- min(leaddata[,2])
 endDate <- max(leaddata[,2])
 # ...or grab just a subset of it
-startDate <- 0 - .5
-endDate <- 100 - .5
+startDate <- -752 - .5
+endDate <- 476 - .5
 # Give the chart an appropriate title
-title <- "First Century"
+title <- "Rome"
 
 startRow <- which(leaddata[,2] == startDate)
 endRow <- which(leaddata[,2] == endDate)
@@ -41,5 +41,15 @@ marks <- seq(-1300, 900, by=spacing)
 
 plot(leaddata[startRow:endRow,2], leaddata[startRow:endRow,3], type="l", xlab=colnames(leaddata[2]), ylab=colnames(leaddata[3]), ylim=c(0,ymax), axes=FALSE, main=title)
 abline(v=marks, col="lightgray")
-axis(1, at=marks, las=2, cex.axis=.6, pos=0)
+axis(1, at=marks, las=1, cex.axis=.9, pos=0)
 axis(2, at=seq(0, 5, by=.5), las=2, pos = startDate + 0.5)
+
+# Lines for Punic Wars
+abline(v=c(-263,-240), col="red")
+abline(v=c(-217,-200), col="blue")
+
+# Line for 64 CE
+abline(v=64, col="red")
+
+# Line for least-squares in 2nd c.
+abline(lsfit(leaddata[startRow:endRow,2], leaddata[startRow:endRow,3]), col="red")
